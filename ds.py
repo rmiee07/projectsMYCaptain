07aecff033159832c3b105222dba02b1f29d0dff
rmiee07 <137005069+rmiee07@users.noreply.github.com>
2023-12-24 23:13:18 +0530

    Update ds.py

--- ds.py
+++ ds.py
@@ -1,1 +1,15 @@
+# define three sets
+E = {0, 2, 4, 6, 8};
+N = {1, 2, 3, 4, 5};
 
+#performing set union function
+print("Union of E and N is",E | N)
+
+#performing set intersection
+print("Intersection of E and N is",E & N)
+
+#performing set difference
+print("Difference of E and N is",E - N)
+
+#performing set symmetric difference
+print("Symmetric difference of E and N is",E ^ N)
